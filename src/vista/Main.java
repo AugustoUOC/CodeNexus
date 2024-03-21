90f543969c46bd2dcf69cd862d104743bde8d537
AlejandroAlonsoGomez <aalonsogomez0@uoc.edu>
2024-03-21 19:45:39 +0100

    Subida final MVC

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
